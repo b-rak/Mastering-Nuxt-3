df316d89b1caff417d14c226ca80fa8276fe8427
b-rak <b.tas93@googlemail.com>
2024-01-30 22:15:58 +0100

    update config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,33 @@
+import vsharp from "vite-plugin-vsharp";
+
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt", "@nuxtjs/supabase"],
+  runtimeConfig: {
+    stripeSecret: "",
+    stripeWebhookSecret: "",
+    public: {
+      stripeKey: "",
+    },
+  },
+  nitro: {
+    prerender: {
+      routes: ["/landing"],
+    },
+  },
+  vite: {
+    plugins: [vsharp()],
+  },
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@vueuse/nuxt",
+    "@nuxtjs/supabase",
+    "@pinia/nuxt",
+  ],
+  supabase: {
+    redirectOptions: {
+      login: "/login",
+      callback: "/",
+      exclude: ["/*"], // We implement our own auth behaviour in the auth middleware
+    },
+  },
 });
